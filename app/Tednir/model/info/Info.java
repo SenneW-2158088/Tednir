1dff6aee480c6087188e901865fc4532226e2c9f
Wolf <wolfmoyaers@gmail.com>
2022-10-31 13:23:18 +0100

    update

--- app/Tednir/model/info/Info.java
+++
@@ -1,8 +0,0 @@
-package app.Tednir.model.info;
-
-abstract public class Info {
-    static final enum Types = {FENRIR_INFO};
-    
-    public abstract getType();
-    public abstract getEvent();
-}
